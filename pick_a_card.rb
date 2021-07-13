63220e6f5c0a51e4dd477e08618e88f673ffaa65
throughtronik <to.isla@gmail.com>
2021-07-14 01:54:35 +0300

    Refactored, now card presented as ASCI-ART

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -1,4 +1,15 @@
-values = [*(2..10), 'J', 'Q', 'K', 'A']
-suits = %w[♦ ♠ ♥ ♣]
+require 'colorize'
 
-puts "You've got a #{values.sample} of #{suits.sample}"
+values = [*(2..10), 'J', 'Q', 'K', 'A']
+suits = ['♦'.colorize(:red), '♠'.colorize(:blue), '♥'.colorize(:red), '♣'.colorize(:blue)]
+
+card = <<~CARD
+   ___
+  |#{values.sample}#{suits.sample} |
+  |   |
+  |___|
+CARD
+
+puts "You've got a"
+
+puts card
